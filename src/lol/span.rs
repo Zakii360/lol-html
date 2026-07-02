55bc92fc00e2e998d799bfdc3de4d02f27989311
Zakii <zakii@360-search.com>
2026-07-02 07:38:32 -0400

    Add Position and Span structs with formatting

---
+++ src/lol/span.rs
@@ -0,0 +1,50 @@
+use std::fmt;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
+pub struct Position {
+    pub line: usize,
+    pub column: usize,
+    pub offset: usize,
+}
+
+impl Position {
+    pub fn new(line: usize, column: usize, offset: usize) -> Self {
+        Self {
+            line,
+            column,
+            offset,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Span {
+    pub start: Position,
+    pub end: Position,
+}
+
+impl Span {
+    pub fn new(start: Position, end: Position) -> Self {
+        Self { start, end }
+    }
+
+    pub fn empty() -> Self {
+        Self {
+            start: Position::default(),
+            end: Position::default(),
+        }
+    }
+}
+
+impl fmt::Display for Span {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}:{}-{}:{}",
+            self.start.line,
+            self.start.column,
+            self.end.line,
+            self.end.column
+        )
+    }
+}
